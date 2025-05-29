fbbfc9248ad78c6b4b175f7d61ed67050b106f85
julimejia <davidmejiao43@gmail.com>
2025-05-29 18:16:50 -0500

    map-reduce

---
+++ mapreduce.py
@@ -0,0 +1,27 @@
+from mrjob.job import MRJob
+import csv
+
+class ProductoMasCaroPorCategoria(MRJob):
+
+    def mapper(self, _, line):
+        for row in csv.reader([line]):
+            if row[0] == "id":
+                return  # Saltar encabezado
+            try:
+                category = row[4]
+                price = float(row[2])
+                title = row[1]
+                yield category, (price, title)
+            except:
+                pass
+
+    def reducer(self, category, values):
+        max_price, max_title = 0, ""
+        for price, title in values:
+            if price > max_price:
+                max_price = price
+                max_title = title
+        yield category, f"{max_title} (${max_price})"
+
+if __name__ == '__main__':
+    ProductoMasCaroPorCategoria.run()
---
+++ readme.md
@@ -0,0 +1,8 @@
+1 The data source 
+
+We taked the data from https://fakestoreapi.com/docs#tag/Products
+
+2 map reduce function 
+
+
+We based our map reduce function in this article https://medium.com/datable/beginners-guide-for-mapreduce-with-mrjob-in-python-dbd2e7dd0f86
